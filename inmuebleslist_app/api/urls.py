d49330d218d55e4c8a99c7b6cc5436963809a302
Jose Barco <jbarco00@hotmail.com>
2024-03-06 19:55:23 -0500

    apis URL

--- inmuebleslist_app/api/urls.py
+++ inmuebleslist_app/api/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from inmuebleslist_app.api.views import ComentarioDetail, ComentarioList, EdificacionDetalleAV, EdificacionListAV, EmpresaAV, EmpresaDetalleAV
 
 urlpatterns = [
-    path('list/', EdificacionListAV.as_view(), name= 'edificacion-list'),
-    path('<int:pk>', EdificacionDetalleAV.as_view(), name= 'edificacion-detail'),
+    path('edificacion/', EdificacionListAV.as_view(), name= 'edificacion-list'),
+    path('edificacion/<int:pk>', EdificacionDetalleAV.as_view(), name= 'edificacion-detail'),
     path('empresa/', EmpresaAV.as_view(), name= 'empresa'),
     path('empresa/<int:pk>', EmpresaDetalleAV.as_view(), name= 'empresa-detail'),
-    path('comentario/', ComentarioList.as_view(), name= 'comentario-list'),
-    path('comentario/<int:pk>', ComentarioDetail.as_view(), name= 'comentario-detail'),
+    path('edificacion/<int:pk>/comentario/', EdificacionDetalleAV.as_view(), name= 'comentario-list'),
+    path('edificacion/comentario/<int:pk>', ComentarioDetail.as_view(), name= 'comentario-detail'),
 ]

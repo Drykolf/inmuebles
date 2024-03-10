df7d4daf2611589d732dd69b721ccac583e635e0
Jose Barco <jbarco00@hotmail.com>
2024-03-10 15:03:28 -0500

    new user toke generation

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -2,13 +2,22 @@ from user_app.api.serializers import RegistrationSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authtoken.models import Token
+from user_app import models
 
 @api_view(['POST',])
 def registration_view(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
+        data = {}
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            account = serializer.save()
+            data['response'] = 'El registro del usuario fue exitoso'
+            data['username'] = account.username
+            data['email'] = account.email
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+            return Response(data, status=status.HTTP_201_CREATED)
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            data = serializer.errors
+            return Response(data, status=status.HTTP_201_CREATED)

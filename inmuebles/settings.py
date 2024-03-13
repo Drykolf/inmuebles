ee226f9053e6245fbdea03e887b2f85955d74ddf
Jose Barco <jbarco00@hotmail.com>
2024-03-13 16:33:11 -0500

    global throttling

--- inmuebles/settings.py
+++ inmuebles/settings.py
@@ -133,7 +133,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ]
+    ],
+    # 'DEFAULT_THROTTLE_CLASSES': [
+    #     'rest_framework.throttling.AnonRateThrottle',
+    #     'rest_framework.throttling.UserRateThrottle',
+    # ],
+    'DEFAULT_THROTTLE_RATES':{
+        'anon': '5/day',
+        'user': '10/day',
+    }
 }
 
 SIMPLE_JWT = {

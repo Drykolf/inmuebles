4a1436fb72ddd46f291d3ea6a101e918e2b0416f
Jose Barco <jbarco00@hotmail.com>
2024-03-15 15:53:48 -0500

    email login

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib import auth
 
 @api_view(['POST',])
 def logout_view(request):
@@ -21,6 +22,9 @@ def registration_view(request):
             data['response'] = 'El registro del usuario fue exitoso'
             data['username'] = account.username
             data['email'] = account.email
+            data['first_name'] = account.first_name
+            data['last_name'] = account.last_name
+            data['phone_number'] = account.phone_number
             #token = Token.objects.get(user=account).key
             #data['token'] = token
             refresh = RefreshToken.for_user(account)
@@ -32,3 +36,27 @@ def registration_view(request):
         else:
             data = serializer.errors
             return Response(data, status=status.HTTP_201_CREATED)
+        
+@api_view(['POST'])
+def login_view(request):
+    data = {}
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+    account = auth.authenticate(email=email, password=password)
+    if account is not None:
+        data['response'] = 'El Login fue exitoso'
+        data['username'] = account.username
+        data['email'] = account.email
+        data['first_name'] = account.first_name
+        data['last_name'] = account.last_name
+        data['phone_number'] = account.phone_number
+        refresh = RefreshToken.for_user(account)
+        data['token'] = {
+            'refresh' : str(refresh),
+            'access' : str(refresh.access_token)
+        }
+        return Response(data)
+    else:
+        data['response'] = 'El Login no fue exitoso'
+        return Response(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
